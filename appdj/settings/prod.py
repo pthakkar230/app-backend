24b943b86f7f92b57803b182bef5f28c24ae668b
Greg Werner <werner.greg@gmail.com>
2017-04-20 12:13:39 -0400

    added TBS_HOST env var
    Signed-off-by: Greg Werner <werner.greg@gmail.com>

--- appdj/settings/prod.py
+++ appdj/settings/prod.py
@@ -1,5 +1,7 @@
 from .base import *
 
-ALLOWED_HOSTS = ['go-pilot.3blades.io', 'localhost', '192.168.0.102', '192.168.43.73']
+ALLOWED_HOSTS = ['go-pilot.3blades.io', 'localhost']
+if 'TBS_HOST' in os.environ:
+    ALLOWED_HOSTS.append(os.environ['TBS_HOST'])
 
 LOGIN_REDIRECT_URL = '/swagger/'
