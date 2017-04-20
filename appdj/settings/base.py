71c906c1dc584b8ac2e06b48fa17cc5dda04a11a
Greg Werner <werner.greg@gmail.com>
2017-04-20 15:28:23 -0400

    Dev setup fixes (#67)
    * Signed off by: Greg Werner <werner.greg@gmail.com>
    
    * ansible/varant dev setup
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * updated instructions
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * run vagrant with virtualenv python2.7
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * remove scripts
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * remove activation scripts
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * ignore vagrant folder
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * vagrantfile cleanup
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * docker run fixes
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * port forwarding for localhost
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * readme updates
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * remove vagrant folder
    
    Signed-off-by: Greg Werner <gregwerner@Gregs-MacBook-Pro.local>
    
    * windows setup vagrant users
    
    Signed-off-by: Greg Werner <gregwerner@Gregs-MacBook-Pro.local>
    
    * readme spacing
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * setup clarifications for win10
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * add virtualbox as option
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * add port to runserver
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * deploy full stack with docker, troubleshooting
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * remove second DOCKER_HOST
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * additional trouble shooting steps
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * changed pip to pip3
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * docker-py work around
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * slackin link update
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * docker-compose, docker install updates
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * added TBS_HOST env var
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * added TBS_HOST env var
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * settings for static assets in s3
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * static assets in server
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * sudo docker-compose
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>

--- appdj/settings/base.py
+++ appdj/settings/base.py
@@ -170,7 +170,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = '{}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
 STATIC_URL = "https://{}/".format(AWS_S3_CUSTOM_DOMAIN)
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
