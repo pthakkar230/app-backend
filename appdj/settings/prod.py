81bfc851ef3259d907ff5f714743c496d8884eb3
jkp85 <jkp85@users.noreply.github.com>
2017-04-21 14:13:00 +0200

    fixes (#69)
    * Fix dev stack startup
    
    Signed-off-by: Jan Kowalski <jankowalskipraca85@gmail.com>
    
    * change openresty image
    
    Signed-off-by: Jan Kowalski <jankowalskipraca85@gmail.com>
    
    * fix typo
    
    Signed-off-by: Jan Kowalski <jankowalskipraca85@gmail.com>
    
    * remove openresty volumes
    
    Signed-off-by: Jan Kowalski <jankowalskipraca85@gmail.com>
    
    * Add spawner tests, some fixes
    
    Signed-off-by: Jan Kowalski <jankowalskipraca85@gmail.com>
    
    * Bump django version
    
    Signed-off-by: Jan Kowalski <jankowalskipraca85@gmail.com>
    
    * Change static files handling
    
    Signed-off-by: Jan Kowalski <jankowalskipraca85@gmail.com>

--- appdj/settings/prod.py
+++ appdj/settings/prod.py
@@ -5,3 +5,6 @@ if 'TBS_HOST' in os.environ:
     ALLOWED_HOSTS.append(os.environ['TBS_HOST'])
 
 LOGIN_REDIRECT_URL = '/swagger/'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
