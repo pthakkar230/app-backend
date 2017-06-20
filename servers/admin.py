b542d63a2c31d40c7f6018310ff53c7c9b8ceffb
Jan Kowalski <jankowalskipraca85@gmail.com>
2017-06-20 18:18:32 +0200

    Add environment type admin pages 3Blades/react-frontend#83
    Signed-off-by: Jan Kowalski <jankowalskipraca85@gmail.com>

--- servers/admin.py
+++ servers/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+
+from .models import EnvironmentResource
+
+
+@admin.register(EnvironmentResource)
+class EnvironmentResourceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cpu', 'memory', 'active')
+    list_filter = ('active',)
